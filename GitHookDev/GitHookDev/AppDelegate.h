83b4cb9dca27bb3b9f0d86beda5006c59b859cc5
hong.zhu <hong.zhu@ucarinc.com>
2018-11-07 14:59:45 +0800

    zhuhong_coderhg091

---
+++ GitHookDev/GitHookDev/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  GitHookDev
+//
+//  Created by ZhuHong on 2018/11/7.
+//  Copyright © 2018年 CoderHG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property(strong, nonatomic)UIWindow *window1;
+@property(strong, nonatomic)UIWindow *window2;
+@property(strong, nonatomic)UIWindow *window3;
+
+
+@end
---
+++ GitHookDev/GitHookDev/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  GitHookDev
+//
+//  Created by ZhuHong on 2018/11/7.
+//  Copyright © 2018年 CoderHG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
